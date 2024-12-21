12663091f1f2a2f52ec69efcdca49f0e2f6839df
bernardinoleite <bernardinoleite2008@gmail.com>
2024-12-21 16:51:18 +0100

    adiciona dtos de user

---
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,15 @@
+
+interface ICreateUserDTO {
+
+    name: string;
+    username: string
+    password: string
+    email: string
+    driver_license: string
+
+}
+
+
+export {
+    ICreateUserDTO
+}
