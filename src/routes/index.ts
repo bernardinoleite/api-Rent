f4e089214a278f1070b775cbd27e86bfec9efcd2
bernardinoleite <bernardinoleite2008@gmail.com>
2024-12-23 07:37:11 +0100

    adiciona tratamento de Erro

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,12 @@ import { categoriesRoutes } from "./categories.routes";
 import { specificationRoutes } from "./specifications.routes";
 import { usersRouter } from "./users.routes";
 import { authenticateRoutes } from "./authenticate.routes";
-import { AppError } from "../errors/AppError";
 
 const router = Router()
 router.use("/categories", categoriesRoutes)
 router.use("/specifications", specificationRoutes)
 router.use("/users", usersRouter)
 router.use(authenticateRoutes)
-
 
 export {
     router
