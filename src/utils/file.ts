0ad0195c9cfac825b39939ff620b38c3c9631d50
bernardinoleite <bernardinoleite2008@gmail.com>
2024-12-27 10:42:02 +0100

    adiciona as configurações de test com jest

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -3,7 +3,6 @@ export const deleteFile = async (filename: string) => {
     try {
         await fs.promises.stat(filename) //verifica se existe
     } catch (error) {
-        console.log("cai aqui")
         return //caso não existe, sai da aplicação
     }
     await fs.promises.unlink(filename) // remove caso o stat achar
