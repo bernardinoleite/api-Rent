7cda459d702b85b52584c12dd440c753ca287da0
bernardinoleite <bernardinoleite2008@gmail.com>
2024-12-27 11:49:13 +0100

    adiciona repositorio em memoria para trabalhar com os testes

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,27 @@
+import { Category } from "../../entities/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
+
+
+
+export class CategoriesRepositoryInMemory implements ICategoriesRepository {
+    private categories: Category[] = []
+
+    constructor() {
+
+    }
+
+    async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+        const category = new Category()
+        Object.assign(category, { name, description, created_at: new Date() })
+        this.categories.push(category)
+
+    }
+    async list(): Promise<Category[]> {
+        return this.categories
+    }
+    async findByName(name: string): Promise<Category> {
+        const category = this.categories.find(category => category.name === name)
+        return category
+    }
+
+}
